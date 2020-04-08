617edf861e24a060cfeb45e3e4b2b6d0780f55b4
Andre Kurniawan <andrekurniiawan@gmail.com>
2020-04-08 20:47:29 +0700

    Quick Push

--- resources/js/components/user/Post.vue
+++ resources/js/components/user/Post.vue
@@ -1,28 +1,77 @@
 <template>
-  <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+  <div class="post-wrapper p-4 my-4 bg-white">
+    <div class="_wrap-form">
+      <form action class="_bg-form">
+        <div class="form-group">
+          <input type="text" class="form-control" name="title" id="title" placeholder="Title" />
+        </div>
+        <div class="form-group">
+          <textarea
+            name="body"
+            id="body"
+            cols="30"
+            rows="8"
+            class="form-control"
+            placeholder="Body"
+          ></textarea>
+        </div>
+        <button class="btn btn-primary" type="submit">Submit</button>
+      </form>
+    </div>
+    <!-- <form role="form" action="#" method="POST">
+      <div class="card card-outline card-info mb-4">
+        <div class="card-body">
+          <div class="col-lg-6 float-left">
+            <div class="form-group">
+              <label for="category">
+                <em>Category</em>
+              </label>
+              <select class="custom-select" name="category">
+                <option selected>-- Choose category --</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-lg-6 float-right">
+            <div class="form-group">
+              <label for="slug">
+                <em>Post slug</em>
+              </label>
+              <input type="text" class="form-control" id="slug" name="slug" placeholder="post-slug" />
+            </div>
+          </div>
+          <div class="float-right">
+            <button type="submit" id="submit" class="btn btn-primary btn-sm mr-3">Submit</button>
+          </div>
+        </div>
+      </div>
+      <div name="body" id="body"></div>
+      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" class="ml-5"></ckeditor>
+      <input type="hidden" id="bodyvalue" name="body" value />
+    </form>-->
+  </div>
 </template>
 
 <script>
-    import Vue from "vue";
-    import BalloonEditor from '@ckeditor/ckeditor5-build-balloon-block';
-    import CKEditor from '@ckeditor/ckeditor5-vue';
+    // import Vue from "vue";
+    // import BalloonEditor from '@ckeditor/ckeditor5-build-balloon-block';
+    // import CKEditor from '@ckeditor/ckeditor5-vue';
 
-    Vue.use( CKEditor );
+    // Vue.use( CKEditor );
 
     export default {
         name: 'Post',
-        components: {
-            // Use the <ckeditor> component in this view.
-            ckeditor: CKEditor.component
-        },
-        data() {
-            return {
-                editor: BalloonEditor,
-                editorData: '<p>Content of the editor.</p>',
-                editorConfig: {
-                    // The configuration of the editor.
-                }
-            };
-        }
+        // components: {
+        //     // Use the <ckeditor> component in this view.
+        //     ckeditor: CKEditor.component
+        // },
+        // data() {
+        //     return {
+        //         editor: BalloonEditor,
+        //         editorData: '<p>Content of the editor.</p>',
+        //         editorConfig: {
+        //             // The configuration of the editor.
+        //         }
+        //     };
+        // }
     }
 </script>
