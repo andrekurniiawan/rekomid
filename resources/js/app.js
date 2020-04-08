6a26154a697b194d0b259083c8deefbe7f3f5b04
Andre Kurniawan <andrekurniiawan@gmail.com>
2020-04-08 07:28:50 +0700

    Quick Push

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import VueRouter from "vue-router";
 import Guest from "./components/Guest.vue";
 
 require("./bootstrap");
+require("admin-lte");
 window.Vue = require("vue");
 
 Vue.use(VueRouter);
